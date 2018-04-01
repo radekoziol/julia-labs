7676f5a5a3dbe4977e80fe5ba98c384880998871
Radek Kozioł <radekoziol.it@gmail.com>
2018-04-01 11:15:36 +0200

    Type corrected and new fun added

--- lab2/algebraic_group/algebraic_group.jl
+++ lab2/algebraic_group/algebraic_group.jl
@@ -1,6 +1,8 @@
 include("../utils.jl")
 import Base.*
-
+import Base.convert
+import Base.promote_type
+import Base.^
 
 N = 12
 
@@ -13,13 +15,61 @@ function check_condition(x)
     end
 end
 
-struct Gn
-        x::Int64
-        Gn(x) = check_condition(x) == 0 ? throw(DomainError()) : new(x)
-        *(x::Gn, y::Gn) = Gn((x.x*y.x)%N)
-        convert(::Type{Gn}, z::Int64) = Gn(z%N)
-        convert(::Int64, z::Type{Gn}) = z.z
+# korzystając z poprzedniej funkcji napisać funkcję obliczającą okres danej
+#  liczby typu G{N} czyli najmniejszą liczbę naturalną r, taką, że a^r mod N =1.
+#  Wskazówka: można to zrobić sprawdzająć po kolei wszystkie możliwości, można też 
+#  skorzystać z twierdzenia, że r musi dzielić ilość elementów w grupie.
+function period(gn::Gn)
+    it = 1
+    while(it != N+1)
+        if(gn^it == 1)
+            return it
+        end
+        it = it + 1
+    end
 end
 
+function reversed(gn:gn)
+    it = 1
+    while(it != gn.x + 1)
+        if((gn.x*it)%N == 1)
+            return Gn(it)
+        end
+        it = it + 1
+    end 
+end
+
+function ^(gn::Gn{Int64}, x::Int64)
+
+    a = promote_type(gn)
+
+    sum = 0
+    while(x != 0)
+        sum = sum + a*a
+        sum = sum%N
+        x = x - 1
+    end
+
+    return Gn(sum)
+end
+
+function group_size(gn::Type{Gn{}})
+    
+    it = 0
+    while gn(it).x == it
+        it = it + 1
+    end
+
+    return it
+end
+
+struct Gn{T} 
+        x::T
+        Gn(x::T) = check_condition(x) == 0 ? error("Can not convert!") : new(x%N)
+        *(x::Gn{Int64}, y::Gn{Int64}) = Gn{Int64}((x.x*y.x)%N)
+        convert(::Type{Gn{Int64}}, z::Int64) = Gn{Int64}(z)
+        convert(::Int64, z::Gn{Int64}) = z.x
+        promote_type(x::Gn{Int64}) = promote_type(x.x)
+end
 
 
