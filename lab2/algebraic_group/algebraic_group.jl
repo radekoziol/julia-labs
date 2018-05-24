2929c1389601b4b76fe5cd14d0ca53f51dc92143
Radek Kozioł <radekoziol.it@gmail.com>
2018-05-24 12:55:29 +0200

    Algebraic group fixed

--- lab2/algebraic_group/algebraic_group.jl
+++ lab2/algebraic_group/algebraic_group.jl
@@ -5,16 +5,31 @@ import Base.promote_type
 import Base.^
 
 # Example
-N = 11
 
-struct Gn{T} 
-    x::T
-    Gn(x::T) = typeof(x) != T || check_condition(x::Int) == 0 ? error("Can not convert!") : new(x%N)
-    *(x::Gn{Int64}, y::Gn{Int64}) = Gn{Int64}((x.x*y.x)%N)
+struct Gn{N}
+    x::Integer
+    function Gn{N}(x) where N 
+         return !has_shared_divisors(x%N,N) ? error("Can not convert!") : new(x%N)
+    end
     convert(::Type{Gn{Int64}}, z::Int64) = Gn{Int64}(z)
     convert(::Int64, z::Gn{Int64}) = z.x
     promote_type(x::Gn{Int64}) = promote_type(x.x)
 end
+
+function *(g1::Gn, g2::Gn) 
+    return typeof(g1)(g1.x*g2.x)
+end
+
+function has_shared_divisors(a,b)
+
+    while b != 0
+        a,b = b, a%b
+    end
+
+    return a == 1
+
+end
+
 
 
 function check_condition(x)
@@ -27,7 +42,7 @@ function check_condition(x)
 end
 
 
-function period(gn::Gn{Int64})
+function period(gn::Gn{N}) where N
     it = 2
     while(it != N)
         if((gn^it).x == 1)
@@ -48,11 +63,12 @@ function reversed(gn::Gn{Int64})
     end 
 end
 
-function ^(gn::Gn{Int64},b::Int64)
+function ^(gn::Gn{N},b::Int64)
 
-    sum = gn
+    sum = gn.x
     while(b > 1)
-        sum = sum*gn
+        sum = sum*gn.x
+        sum %= N
         b = b - 1
     end
 
@@ -69,4 +85,22 @@ function group_size(gn::Type{Gn{T}}) where T <:Int
 
     return it
 end
+
+
+N=55
+c=17
+b=4
+#Calculate period r of message b in a multiplicative algebraic group Gn{N}
+r = period(Gn{N}(b))
+println("repetition period = ",r)
+#Calculate d - modular multiplicative inverse of c in Gn{r}. It's a private key
+d = Int16(reversed(c,N)[2])
+println("Inveresed element = ", d)
+#Decoded message
+a = b^d %N
+#Check if the coding (N,k) is correct
+println("Decoded msg: ", a)
+println("a^c % N = ",a^c % N)
+println("b = ", b)
+println(a^c % N == b ? "Coding is correct" : "Coding is wrong")
 
